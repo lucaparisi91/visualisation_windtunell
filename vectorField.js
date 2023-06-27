05841af413a3bfc6bec62a62abb7a45ef296f98d
Luca Parisi <lparisi@MBP-LP.local>
2023-06-27 17:56:49 +0100

    Rendering of external particle arrays. Positions are scaled to the size of the container

--- vectorField.js
+++ vectorField.js
@@ -1,48 +1,71 @@
 
 import { windtunnelField, field2D } from "./getField.js"
 
-const renderParticles = function()
+
+
+
+
+const renderParticles = function(initialPositions)
 {
     const path = new paper.Path();
     // // Give the stroke a color
     path.strokeColor = 'black';
-    
-    
+
     const dot_path = new paper.Path.Circle(new paper.Point(100, 70), 5);
     dot_path.fillColor = 'green';
     dot_path.opacity = 0.2
-    const nParticles = 1000
+    const nParticles = initialPositions.length
 
 
     const velocities= Array.from(Array(nParticles).keys()).map ((i) => [ Math.random()*0.01 , Math.random()*0.01 ] )
 
-    const initialPositions = Array.from(Array(nParticles).keys()).map ((i) => [ Math.random()*paper.view.size.width, Math.random()*paper.view.size.height]   )
 
 
     const dot_symbol = new paper.Symbol(dot_path)
+
+    const xExtent= d3.extent( initialPositions, (d)=>d[0] )
+    const yExtent= d3.extent( initialPositions, (d)=>d[1] )
+
     
+    const xScale=d3.scaleLinear( ).domain(xExtent).range([0,paper.view.size.width])
+    const yScale=d3.scaleLinear( ).domain(yExtent).range([paper.view.size.height,0])
     
-    
-    
+
     const dots = []
+
 
     for(let i=0;i<nParticles;i++)
     {
-        const x = initialPositions[i][0]
-        const y = initialPositions[i][1]
+        const x = xScale(initialPositions[i][0] )
+        const y = yScale(initialPositions[i][1] )
+
         
         dots.push( dot_symbol.place( new paper.Point(x,y ))  )
 
     }
 
+    const pbc=(x,L) =>{
+        if (x>L)
+        {
+            return x-L
+        }
+        else if (x<0)
+        {
+            return x+L
+        }
+        else 
+        {
+            return x
+        }       
+    }
+
     paper.view.onFrame=function(event) {    
         
-        for ( var i =0 ; i < nParticles ; i++)
+        for ( let i =0 ; i < nParticles ; i++)
         {
-            dots[i].position.x= ( dots[i].position.x + velocities[i][0]*paper.view.size.width ) % paper.view.size.width;
+            dots[i].position.x= pbc( dots[i].position.x + velocities[i][0]*paper.view.size.width , paper.view.size.width);
 
-            dots[i].position.y= ( dots[i].position.y + velocities[i][1]*paper.view.size.height ) % paper.view.size.height;
-            
+            dots[i].position.y=pbc( dots[i].position.y + velocities[i][1]*paper.view.size.height , paper.view.size.height);
     
         }
     }
@@ -57,7 +80,7 @@ const generateRandomPositions=function(nParticles,extent)
     const xScale = d3.scaleLinear().domain([0,1]).range(extent[0])
     const yScale = d3.scaleLinear().domain([0,1]).range(extent[1])
 
-    return initialPositions.map((x) => [ xScale(x[0]) , xScale(x[1]) ] )
+    return initialPositions.map((x) => [ xScale(x[0]) , yScale(x[1]) ] )
 
 
 }
@@ -72,7 +95,9 @@ window.onload = function() {
     const initialPositions=generateRandomPositions(nParticles,extent)
 
 
-    renderParticles()
+
+
+    renderParticles(initialPositions)
 
 }
 
