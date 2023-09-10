71028e96501f3147820725bbbb2b8a08e37ad87e
lparisi91 <luca.parisi91@gmail.com>
2023-09-10 07:23:08 +0100

    Multiple tracks on the same figure with groupby

--- react_sandbox/index.js
+++ react_sandbox/index.js
@@ -11,17 +11,18 @@ import * as d3 from "d3";
 const domNode= document.getElementById("chart_control")
 const root = createRoot(domNode)
 
-const data = await d3.csv("benchio_unstriped_hdf5.csv")
+const data = await d3.csv("benchio_unstriped_hdf5_all_filesystems.csv")
+const dataGrouped= d3.group( data, (d) => d.name )
 
 
 const fig = new Figure()
-const chart = new perfChart(data,fig)
+const chart = new perfChart( dataGrouped ,fig)
+
 
 root.render( <App onDateChange= { (time1,time2) => { 
     chart.xlim(time1,time2)
      } } />)
 
-
-const dataGrouped= d3.group( data, (d) => d.name )
-
 dataGrouped.forEach( (value,key) => {console.log(value)} )
+
+
